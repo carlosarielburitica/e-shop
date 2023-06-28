eb4e68dae2e1cd15bfac65c15c598c00ade181c2
carlosarielburitica <70611154+carlosarielburitica@users.noreply.github.com>
2023-06-27 23:00:41 -0500

    Agregar archivos de proyecto.

---
+++ e_store.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_store.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
